032639ccc6e9b79adf5b1d7d02d26f162c5e3562
dkmakyer <kwasiakyer2105@gmail.com>
2025-01-18 13:26:27 +0000

    --

--- src/components/sales-container/SalesContainer.jsx
+++ src/components/sales-container/SalesContainer.jsx
@@ -15,12 +15,13 @@ const SalesContainer = ({
   payout,
   gross,
   net,
+  topLoss,
+  downLoss
 }) => {
   const [display, setDisplay] = useState(false);
 
   const toggleDisplay = () => {
     setDisplay((prev) => !prev);
-    // alert("triggered");
   };
 
   const currentDate = new Date().toLocaleDateString();
@@ -58,14 +59,14 @@ const SalesContainer = ({
 
   return (
     <div className="sales-container relative w-[20%] flex flex-col">
-      <div className={`sales-container-top ${bg} text-white p-2`}>
+      <div className={`sales-container-top ${bg} text-white p-2 `}>
         <h2>{time}</h2>
         <h3 className="text-[13px]">{date}</h3>
       </div>
       <div className="sales-container-middle p-2">
-        <div className="mini-middle pb-2">
-          <p className="text-gray-600">Sales</p>
-          <h1 className="text-2xl font-bold">${sales}</h1>
+        <div className="mini-middle text-[13px] pb-2">
+          <p className="text-gray-600 w-[50%] flex items-center justify-between">Sales <span className={`text-[12px] ${topLoss.split("")[0] === "-" ? "text-red-500" : "text-green-500"}`}>{topLoss}</span></p>
+          <h1 className="text-2xl font-bold mb-[0.5rem]">${sales}</h1>
           <div className="order-details flex flex-row justify-between">
             <div className="order">
               <p className="text-gray-600">Order / Units</p>
@@ -81,7 +82,7 @@ const SalesContainer = ({
         </div>
       </div>
       <div className="pricing p-2">
-        <div className="mini-pricing pb-2 grid grid-cols-2">
+        <div className="mini-pricing text-[13px]  pb-2 grid grid-cols-2">
           <div className="cost">
             <p className="text-gray-600">Adv. cost</p>
             <h4 className="text-[0.9rem] font-bold">$-{cost}</h4>
@@ -95,12 +96,12 @@ const SalesContainer = ({
             <h4 className="text-[0.9rem] font-bold">${gross}</h4>
           </div>
           <div className="net-profit">
-            <p className="text-gray-600">Net profit</p>
+            <p className="text-gray-600  flex items-center justify-between">Net profit <span className={`text-[12px] ${downLoss.split("")[0] === "-" ? "text-red-500" : "text-green-500"}`}>{downLoss}</span></p>
             <h4 className="text-[0.9rem] font-bold">${net}</h4>
           </div>
         </div>
         <button
-          className="more-button pt-[0.2rem] pl-[5rem] text-blue-700 pb-[10px]"
+          className="more-button w-[100%] pt-[0.5rem] pl-[8%] text-blue-700 pb-[10px]"
           onClick={toggleDisplay}
         >
           More
